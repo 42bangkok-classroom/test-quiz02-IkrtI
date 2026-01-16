b21d1768683e5162a674fb0adc9cd57bbf191748
Ikrt <me@ikrt.dev>
2026-01-16 18:45:38 +0700

    add axios dependency and implement getPostalAddress function

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,1 +1,40 @@
-export function getPostalAddress() {}
+import axios from "axios";
+
+export function getPostalAddress() {
+  return axios("https://jsonplaceholder.typicode.com/users")
+    .then(({ data }): user[] =>
+      data.map((user: user) => ({
+        id: user.id,
+        name: user.name,
+        phone: user.phone,
+        address: user.address || null,
+      }))
+    )
+    .catch((err) => {
+      return err;
+    });
+}
+
+interface user {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  } | null;
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
